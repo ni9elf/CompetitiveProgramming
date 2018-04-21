f57cd0a7363b6efaedf6bf9c580534beb612680b
Nigel Fernandez <ni9elf@gmail.com>
2018-04-21 19:00:00 +0530

    added leetcode 274

---
+++ Leetcode/274_h_index.cc
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+        int index = 0;
+        for (int i = 0; i < citations.size(); ++i)
+            if (citations[i] >= i+1)
+                index += 1;        
+        return index;
+    }
+};
