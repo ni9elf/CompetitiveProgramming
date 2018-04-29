4fcf4c7dc221dda534f427aa3e9d9aeb488b5797
Nigel Fernandez <ni9elf@gmail.com>
2018-04-29 20:26:56 +0530

    added bit_manipulation_lonely_integer

---
+++ HackerRank/CrackingTheCodingInterview/bit_manipulation_lonely_integer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> val;
+        res = res ^ val;
+        }
+    cout << res;
+    
+    return 0;
+    }
