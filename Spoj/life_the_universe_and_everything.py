49fe075c1039a5784482fdff2661b9cf8d694340
Nigel Fernandez <ni9elf@gmail.com>
2018-05-08 21:01:48 +0530

    added spoj life_the_universe_and_everything.py

---
+++ Spoj/life_the_universe_and_everything.py
@@ -0,0 +1,8 @@
+#http://www.spoj.com/problems/TEST/
+
+if __name__ == '__main__':
+    while( True ):
+        num = int(raw_input())
+        if(num == 42):
+            break
+        print( num )        
